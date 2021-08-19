2e1a92fb04314d38a13008ec7f150e0c5113e858
ewenmic <ewenmic@gmail.com>
2021-08-18 21:49:24 -0400

    created initial homeownerApplications db connection

--- src/app/services/homeowner-application.service.ts
+++ src/app/services/homeowner-application.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as firebase from "firebase"
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+export interface Application {
+  name: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeownerApplicationService {
+  private homeownerApplication: AngularFirestoreCollection<Application>;
+  private applications: Observable<Application[]>;
 
-  constructor() { }
+  constructor(
+    private db: AngularFirestore,
+    ) {
+      this.homeownerApplication = db.collection<Application>('homeownerApplications');
+      this.applications = this.homeownerApplication.snapshotChanges().pipe(
+        map(actions => {
+          return actions.map(a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          });
+        })
+      );
+    }
+
+    getApplications() {
+      return this.applications;
+    }
+
+
+
+
 }
