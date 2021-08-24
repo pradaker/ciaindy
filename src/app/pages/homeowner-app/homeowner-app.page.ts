9c4ff468f5535991028cd7be6b6a294bc4484fe1
pradaker <pradaker7@gmail.com>
2021-08-23 20:26:46 -0400

    Started adding form stuff

--- src/app/pages/homeowner-app/homeowner-app.page.ts
+++ src/app/pages/homeowner-app/homeowner-app.page.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-homeowner-app',
   templateUrl: './homeowner-app.page.html',
   styleUrls: ['./homeowner-app.page.scss'],
 })
+
 export class HomeownerAppPage implements OnInit {
 
-  constructor() { }
+  public gymDetailsForm: FormGroup;
+
+  constructor() {}
 
   ngOnInit() {
+    this.createForm()
+  }
+
+  createForm() {
+    this.gymDetailsForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  saveDetail() {
+    console.log(this.gymDetailsForm.controls.name.value)
+  }
+
+  public getGymDetailsFormErrors(): string[] {
+    const errors = [];
+
+    const nameControl = this.gymDetailsForm.controls.name;
+    if (nameControl.dirty && nameControl.hasError('required')) {
+      errors.push('The Name field is required');
+    }
+
+    const addressControl = this.gymDetailsForm.controls.address;
+    if (addressControl.dirty && addressControl.hasError('required')) {
+      errors.push('The Address field is required');
+    }
+
+    const emailControl = this.gymDetailsForm.controls.email;
+    if (emailControl.dirty && emailControl.errors) {
+      if (emailControl.hasError('required')) {
+        errors.push('The Email field is required');
+      }
+
+      if (emailControl.hasError('email')) {
+        errors.push('The Email format is wrong.');
+      }
+    }
+
+    return errors;
   }
 
 }
