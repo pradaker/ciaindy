22675c8b45bf0393b602255bde4d7b50c50b4fbd
pradaker <pradaker7@gmail.com>
2021-12-30 13:17:45 -0500

    Got the "Get Involved" content editing page working.

--- src/app/admin/editgetinvolved/editgetinvolved.module.ts
+++ src/app/admin/editgetinvolved/editgetinvolved.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,11 +8,22 @@ import { EditgetinvolvedPageRoutingModule } from './editgetinvolved-routing.modu
 
 import { EditgetinvolvedPage } from './editgetinvolved.page';
 
+import { QuillModule } from 'ngx-quill';
+
+import { RichTextEditorModule } from '../rich-text-editor/rich-text-editor.module';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
+    RichTextEditorModule,
     IonicModule,
+    QuillModule.forRoot({
+      modules: {
+        syntax: true
+      }
+    }),
     EditgetinvolvedPageRoutingModule
   ],
   declarations: [EditgetinvolvedPage]
--- src/app/admin/editgetinvolved/editgetinvolved.page.ts
+++ src/app/admin/editgetinvolved/editgetinvolved.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { GetInvolvedContent, GetInvolvedContentService } from 'src/app/services/get-involved-content.service';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-editgetinvolved',
@@ -7,9 +11,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditgetinvolvedPage implements OnInit {
 
-  constructor() { }
+  GetInvolvedContents: GetInvolvedContent[] = [];
+
+  public getInvolvedContentForm: FormGroup;
+
+  constructor(
+    private alertController: AlertController,
+    private formBuilder: FormBuilder,
+    private GetInvolvedContentService: GetInvolvedContentService,
+    private firestore: AngularFirestore,
+  ) { }
 
   ngOnInit() {
+    this.createForm()
+    this.getGetInvolvedContent()
+  }
+
+  async getGetInvolvedContent() {
+    this.GetInvolvedContentService.getGetInvolvedContent().subscribe(async (res: any) => {
+      console.log(res)
+      console.log(res[0])
+      let i = 0
+      await res.forEach((contentArea: any) => {
+        this.getInvolvedContentForm.patchValue({ ['description'+i]: contentArea.description })
+        this.getInvolvedContentForm.patchValue({ ['title'+i]: contentArea.title })
+        i += 1
+      })
+    })
+  }
+
+  createForm() {
+    this.getInvolvedContentForm = this.formBuilder.group({
+      title0: [Validators.required],
+      description0: [],
+      title1: [Validators.required],
+      description1: [],
+      title2: [Validators.required],
+      description2: []
+    });
+  }
+
+  get descriptionGetInvolvedPage() {
+    return this.getInvolvedContentForm.controls.description as FormControl;
+  }
+
+  async saveForm(number) {
+    const title = this.getInvolvedContentForm.value['title'+number].toString()
+    const description = this.getInvolvedContentForm.value['description'+number].toString()
+    console.log(title, description)
+    const alert = await this.alertController.create({
+      header: 'Save complete',
+      message: 'Content has been successfully saved.',
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Got it!',
+          handler: () => {
+            console.log('Content saved successfully');
+          }
+        }
+      ]
+    });
+    await alert.present();
+    // Save data to firestore
+    this.firestore.collection('getInvolvedContent').doc('FieCCjgM9Bu6oTZaPr5T').update({title: title}).then(() => console.log('Title success')).catch(err => console.error(err))
+    this.firestore.collection('getInvolvedContent').doc('FieCCjgM9Bu6oTZaPr5T').update({description: description}).then(() => console.log('Description success')).catch(err => console.error(err))
   }
 
 }
