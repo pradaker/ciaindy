f774bc35450963a75c33a07d7c680ec49a422cfb
pradaker <pradaker7@gmail.com>
2021-12-30 13:43:21 -0500

    Made the page to edit the footer work

--- src/app/admin/editfooter/editfooter.page.ts
+++ src/app/admin/editfooter/editfooter.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { FooterContent, FooterContentService } from 'src/app/services/footer-content.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-editfooter',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditfooterPage implements OnInit {
 
-  constructor() { }
+  FooterContents: FooterContent[] = [];
+
+  public footerContentForm: FormGroup;
+
+  constructor(
+    private alertController: AlertController,
+    private formBuilder: FormBuilder,
+    private FooterContentService: FooterContentService,
+    private firestore: AngularFirestore,
+  ) { }
 
   ngOnInit() {
+    this.createForm()
+    this.getFooterContent()
+  }
+
+  async getFooterContent() {
+    this.FooterContentService.getFooterContent().subscribe(async (res: any) => {
+      console.log(res)
+      console.log(res[0])
+      let i = 0
+      await res.forEach((contentArea: any) => {
+        this.footerContentForm.patchValue({ ['description'+i]: contentArea.description })
+        this.footerContentForm.patchValue({ ['title'+i]: contentArea.title })
+        i += 1
+      })
+    })
+  }
+
+  createForm() {
+    this.footerContentForm = this.formBuilder.group({
+      text: [Validators.required],
+    });
+  }
+
+  get descriptionFooterPage() {
+    return this.footerContentForm.controls.description as FormControl;
+  }
+
+  async saveForm() {
+    const text = this.footerContentForm.value['text'].toString()
+    console.log(text)
+    const alert = await this.alertController.create({
+      header: 'Save complete',
+      message: 'Content has been successfully saved.',
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Got it!',
+          handler: () => {
+            console.log('Content saved successfully');
+          }
+        }
+      ]
+    });
+    await alert.present();
+    // Save data to firestore
+    this.firestore.collection('footerContent').doc('7c3TA9k9P8sshhLHHi0I').update({text: text}).then(() => console.log('Footer text change successful')).catch(err => console.error(err))
   }
 
 }
